8cb5862cc9d062dfd8371a731de433342762f398
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-04-01 20:12:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,24 @@ import '../styles/App.css';
 const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
+  const [API KEY] = useState("0a9a1d28d2d04347a001b6f2059b3150")
+  const [API_URL] = useState(`https://gnews.io/api/v4/top-headlines?apikey=${API_KEY}&
+  max=10&lang=en&category=`)
+  useEffect(( =>{
+             setLoading(true);
+            fetch(`${API_URL}${category}`);
+            .then(response => response.json())
+            .then(data =>{
+            setNewsData(data.articles);
+            setLoading(false);
+            })
+              .catch(error => console.error(error));
+             },[category]);
+             copnst handleCategoryChange = event => {
+             setCategory(event.target.value);
+            };
+            
 
   return (
     <div id="main">
@@ -18,22 +35,27 @@ const App = () => {
         <option value="entertainment">Entertainment</option>
         <option value="science">Science</option>
       </select>
+      {loading ? (
       <p className='loader'>Loading...</p>
+      ):(
       <ol>
-        <li key="">
-          <img className='news-img' src="" alt=""/>
+      {newsData.map((news,index) => (
+        <li key={"index"}>
+          <img className='news-img' src={news.image} alt={news.title}/>
           <section className='new-title-content-author'>
-            <h3 className='news-title'>news title</h3>
+            <h3 className='news-title'>{news title}</h3>
             <section className='new-content-author'>
-              <p className='news-description'>news description</p>
-              <p className='news-source'><strong>Source:</strong> source name</p>
+              <p className='news-description'>{news description}</p>
+              <p className='news-source'><strong>Source:</strong>{news. source name}</p>
             </section>
           </section>
         </li>
+        ))}
       </ol>
+      )}
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
